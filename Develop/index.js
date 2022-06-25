901fbea831f83309d5eb55ff97816a218abda35a
Angie <103882157+anakela@users.noreply.github.com>
2022-06-24 21:32:48 -1000

    Updated the index.js file and created a README.md file

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,71 @@
 // TODO: Include packages needed for this application
 const inquirer = require('Inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = inquirer
+    .prompt([
+    {
+        message: 'What is your project title?',
+        name: 'projName',
+        type: 'input',
+        default: 'Project Title'
+    },
+    {
+        message: 'Please enter a project description.',
+        name: 'projDesc',
+        type: 'input',
+        default: 'This is a description of my project.'
+    },
+    {
+        message: 'Please provide installation instructions for your project.',
+        name: 'projInstall',
+        type: 'input',
+        default: 'These are instructions for my project installation.'
+    },
+    {
+        message: 'Please provide usage information for your project.',
+        name: 'projUse',
+        type: 'input',
+        default: 'This is how we do iiiiit.'
+    },
+    {
+        message: 'Please provide contributation guidelines.',
+        name: 'contGuide',
+        type: 'input',
+        default: 'You must be this tall to contribute.'
+    },
+    {
+        message: 'How does one test your project?',
+        name: 'projTest',
+        type: 'input',
+        default: 'This is the real test.'
+    },
+    {
+        message: 'Please select a license for your application.',
+        name: 'licenseList',
+        type: 'list',
+        choices: [
+            '',
+            '',
+            '',
+            ''
+        ]
+    },
+    {
+        message: 'What is your GitHub username?',
+        name: 'gitHub',
+        type: 'input',
+        default: 'GitHub Username'
+    },
+    {
+        message: 'What is your email address?',
+        name: 'email',
+        type: 'input',
+        default: 'example@example.com'
+    },
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
